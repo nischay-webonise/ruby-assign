d8a2d13f969a9a40886460d0a604f6eaa9118a6c
nischay-webonise <nischay.dhiman@weboniselab.com>
2016-04-19 13:02:59 +0530

    added file write functionality

--- 19th april 2016/13.rb
+++ 19th april 2016/13.rb
@@ -1,9 +1,16 @@
 #!/usr/bin/ruby -w
 
-f = File.open('input.txt', 'r+')if File::exists?('input.txt')
+f = File.new('input.txt', 'r') if File.exist?('input.txt')
 if f
   content = f.sysread(1000)
   puts content
 else
-  puts "not found"
+  puts 'not found'
+end
+
+f = File.new('input1.txt', 'a')
+if f
+  f.syswrite(content)
+else
+  puts 'not found'
 end
